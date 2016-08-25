f73c6eb912418a408c50de9b146816bf55c97ad4
amanjuneja5 <amanjuneja5@gmail.com>
2016-08-25 10:44:25 +0530

    Change Test New

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Testing the comment
+* Testing the comment cont.
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
